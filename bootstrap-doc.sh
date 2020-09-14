db0c9bb57e097c79462f1254cfd21acad34a4d78
Yuri D'Elia <wavexx@thregr.org>
2020-09-14 02:11:35 +0200

    Add G-Code reference extracted from the RepRap Wiki

---
+++ bootstrap-doc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# bootstrap initial descriptions from the RepRap wiki
+curl 'https://reprap.org/mediawiki/api.php?page=G-code&action=parse&prop=wikitext&format=json' | \
+  jq -r '.parse.wikitext."*"|@text' | \
+  sed -ne 's/^====* \([GMTD][^:]\+\): \(.*\) =*=$/\1\t\2/p' | \
+  perl -a -n -F'[\t\n]' -e 'print "(puthash \"$F[0]\" \"$F[1]\" hash)\n"'
